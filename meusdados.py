1af29e49c0f32fe1c465e61e47244d796a4a2d01
Roberto-Kaiser <159036437+Roberto-Kaiser@users.noreply.github.com>
2025-06-04 09:32:41 -0300

    Update meusdados.py

--- meusdados.py
+++ meusdados.py
@@ -5,4 +5,4 @@ else:
     print("Digite a sua senha corretamente para acessar seus dados!")
     senha = input("Digite sua senha:")
     if senha == "Robertao2008#":
-        print("dados")
+        print("Seu dados:\nCpf: 111.111.111-11\nEmail: robisvaldo@gmail.com\nGênero: Masculino\nTelefone: (41)95565-1788")
